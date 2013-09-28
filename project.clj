428ae73e2e8f268d0ec0abd7cc0bf5b025617269
Garrett Thornburg <film42@gmail.com>
2013-09-27 18:32:34 -0600

    update description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mirror-json "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple compojure app that responds with any json data thrown at it."
+  :url "http://mirror-clojure.herokuapp.com/"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [ring/ring-json "0.2.0"]
