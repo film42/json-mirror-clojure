697b9d47ac826779e1ad80582df8621707d3fdce
Garrett Thornburg <film42@gmail.com>
2013-09-28 11:45:43 -0600

    removing the old src dirs, woops

--- src/mirror_json/server.clj
+++
@@ -1,8 +0,0 @@
-(ns mirror-json.server
-  (:use ring.adapter.jetty)
-  (:use mirror-json.handler))
-
-;; Our fancy -main for heroku
-(defn -main [& args]
-	(let [port (Integer/parseInt (get (System/getenv) "PORT" "8080"))]
-    (run-jetty app {:port port})))
